f8226d678f9165cb33e908c5f543bc22516354c0
Karl-Andero Mere <anddero@gmail.com>
2025-04-08 08:09:56 +0000

    wip

--- octofit-tracker/frontend/src/components/Teams.js
+++ octofit-tracker/frontend/src/components/Teams.js
@@ -4,7 +4,7 @@ function Teams() {
   const [teams, setTeams] = useState([]);
 
   useEffect(() => {
-    fetch('https://[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev/api/teams/')
+    fetch('https://vigilant-invention-pvg965vvrxh5jg-8000.app.github.dev/api/teams/')
       .then(response => response.json())
       .then(data => setTeams(data))
       .catch(error => console.error('Error fetching teams:', error));
